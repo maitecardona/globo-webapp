35e51000d596a82c6263857d7db94a7aefdd8c06
Maite Cardona <maite.cardona@avanade.com>
2025-04-17 14:22:27 -0500

    Switch to using secrets manager

--- variables.tf
+++ variables.tf
@@ -32,11 +32,6 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "api_key" {
-  type        = string
-  description = "(Required) API key for web app to talk to SaaS platform."
-}
-
 variable "api_key_secret_id" {
   type        = string
   description = "(Required) Role Name for EC2 instance profile."
